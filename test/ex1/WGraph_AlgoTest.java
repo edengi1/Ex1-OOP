1933a8e7bc52db8f6ab4a97f86e140c44f233de2
edengi1 <74146562+edengi1@users.noreply.github.com>
2020-11-22 23:32:17 +0200

    Update WGraph_AlgoTest.java

--- test/ex1/WGraph_AlgoTest.java
+++ test/ex1/WGraph_AlgoTest.java
@@ -1,6 +1,11 @@
-package ex1;
+package test;
 
 import org.junit.jupiter.api.Test;
+
+import ex1.WGraph_Algo;
+import ex1.node_info;
+import ex1.weighted_graph;
+import ex1.weighted_graph_algorithms;
 
 import java.util.List;
 
@@ -36,45 +41,45 @@ class WGraph_AlgoTest {
         assertTrue(b);
     }
 
-//    @Test
-//    void shortestPathDist() {
-//        weighted_graph g0 = small_graph();
-//        weighted_graph_algorithms ag0 = new WGraph_Algo();
-//        ag0.init(g0);
-//        assertTrue(ag0.isConnected());
-//        double d = ag0.shortestPathDist(0,10);
-//        assertEquals(d, 5.1);
-//    }
+    @Test
+    void shortestPathDist() {
+        weighted_graph g0 = small_graph();
+        weighted_graph_algorithms ag0 = new WGraph_Algo();
+        ag0.init(g0);
+       assertTrue(ag0.isConnected());
+        double d = ag0.shortestPathDist(0,10);
+        assertEquals(d, 5.1);
+    }
 
-//    @Test
-//    void shortestPath() {
-//        weighted_graph g0 = small_graph();
-//        weighted_graph_algorithms ag0 = new WGraph_Algo();
-//        ag0.init(g0);
-//        List<node_info> sp = ag0.shortestPath(0,10);
-//        //double[] checkTag = {0.0, 1.0, 2.0, 3.1, 5.1};
-//        int[] checkKey = {0, 1, 5, 7, 10};
-//        int i = 0;
-//        for(node_info n: sp) {
-//        	//assertEquals(n.getTag(), checkTag[i]);
-//        	assertEquals(n.getKey(), checkKey[i]);
-//        	i++;
-//        }
-//    }
+    @Test
+    void shortestPath() {
+        weighted_graph g0 = small_graph();
+        weighted_graph_algorithms ag0 = new WGraph_Algo();
+        ag0.init(g0);
+        List<node_info> sp = ag0.shortestPath(0,10);
+        //double[] checkTag = {0.0, 1.0, 2.0, 3.1, 5.1};
+        int[] checkKey = {0, 1, 5, 7, 10};
+        int i = 0;
+        for(node_info n: sp) {
+        	//assertEquals(n.getTag(), checkTag[i]);
+        	assertEquals(n.getKey(), checkKey[i]);
+        	i++;
+        }
+    }
     
-//    @Test
-//    void save_load() {
-//        weighted_graph g0 = WGraph_DSTest.graph_creator(10,30,1);
-//        weighted_graph_algorithms ag0 = new WGraph_Algo();
-//        ag0.init(g0);
-//        String str = "g0.obj";
-//        ag0.save(str);
-//        weighted_graph g1 = WGraph_DSTest.graph_creator(10,30,1);
-//        ag0.load(str);
-//        assertEquals(g0,g1);
-//        g0.removeNode(0);
-//        assertNotEquals(g0,g1);
-//    }
+    @Test
+    void save_load() {
+        weighted_graph g0 = WGraph_DSTest.graph_creator(10,30,1);
+        weighted_graph_algorithms ag0 = new WGraph_Algo();
+        ag0.init(g0);
+        String str = "g0.obj";
+        ag0.save(str);
+        weighted_graph g1 = WGraph_DSTest.graph_creator(10,30,1);
+        ag0.load(str);
+        assertEquals(g0,g1);
+        g0.removeNode(0);
+        assertNotEquals(g0,g1);
+    }
 
     private weighted_graph small_graph() {
         weighted_graph g0 = WGraph_DSTest.graph_creator(11,0,1);
